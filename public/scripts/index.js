80b7ade5a754d6618597a4ee85ae6bffea7f7571
Nataliesmyth <nataliesmyth11@gmail.com>
2021-06-11 01:41:00 -0400

    dynamic templating for team

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,10 +1,66 @@
 console.log('sanity check')
+const rootElement = document.getElementById('rootElement');
 
-$('#navbarDropdown').on('show.bs.dropdown', function () {
-    // do something…
-    console.log('hello')
-  })
 
-$('#home-link').on('click', function() {
-  $('#main-index').hide()
-})
+const team = [
+  {name: 'test', role: 'test', image: 'test', bio: 'test'},
+  {name: 'boo', role: 'test', image: 'test', bio: 'test'},
+  {name: 'test', role: 'test', image: 'test', bio: 'test'},
+  {name: 'test', role: 'test', image: 'test', bio: 'test'},
+  {name: 'test', role: 'test', image: 'test', bio: 'test'},
+  {name: 'test', role: 'test', image: 'test', bio: 'test'},
+  {name: 'test', role: 'test', image: 'test', bio: 'test'},
+  {name: 'test', role: 'test', image: 'test', bio: 'test'},
+  {name: 'test', role: 'test', image: 'test', bio: 'test'},
+  {name: 'test', role: 'test', image: 'test', bio: 'test'},
+  {name: 'test', role: 'test', image: 'test', bio: 'test'},
+  {name: 'test', role: 'test', image: 'test', bio: 'test'},
+  {name: 'test', role: 'test', image: 'test', bio: 'test'},
+  {name: 'test', role: 'test', image: 'test', bio: 'test'},
+  {name: 'test', role: 'test', image: 'test', bio: 'test'},
+  {name: 'test', role: 'test', image: 'test', bio: 'test'},
+  {name: 'test', role: 'test', image: 'test', bio: 'test'},
+]
+
+const getTeamTemplate = (props) => {
+  return `
+      <div class="card">
+          <h4>${props.name}</h4>
+          <p>${props.role}</p>
+          <p>${props.image}</p>
+          <p>${props.bio}</p>
+      </div>
+  `;
+};
+
+const renderTeam = (teamArray) => {
+  rootElement.innerHTML = '';
+
+  const teamTemplates = teamArray.map((team) => {
+      return getTeamTemplate(team);
+  }).join('');
+  
+  console.log(teamTemplates)
+
+  rootElement.insertAdjacentHTML('beforeend', teamTemplates);
+}
+
+teamBtn.addEventListener('click', () => {
+  renderTeam(team);
+});
+
+
+
+
+
+
+
+
+// $('#navbarDropdown').on('show.bs.dropdown', function () {
+//     // do something…
+//     console.log('hello')
+//   })
+
+// $('#home-link').on('click', function() {
+//   $('#main-index').hide()
+// })
